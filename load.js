f61160ebb452ec200143b9838c3aef5599fa07df
Ilya <43515223+KlishevichIlya@users.noreply.github.com>
2020-11-19 09:45:15 +0300

    Add files via upload

--- load.js
+++ load.js
@@ -1,21 +1,42 @@
-let url = './make_and_model2.json';
-let response = await fetch(url,{
-	headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-});
+let url = 'https://klishevichilya.github.io/Added.github.io/make_and_model2.json';
+let response = await fetch(url);
 let selectEl = document.getElementById('make-select');
+let selectElMod = document.getElementById('model-select');
 
 if(response.ok){
 	let json = await response.json();	
-	for(const id of Object.getOwnPropertyNames(json))
+	for(let id of Object.getOwnPropertyNames(json))
 	{
 		let maker = json[id];
-		console.log(maker);
+		for(let key in maker){
+			if(key == 'make_name'){
+				let opt = document.createElement('option');
+				opt.appendChild(document.createTextNode(maker[key]));
+				opt.value = key;
+	            selectEl.appendChild(opt);
+        	}
+		}
 	}
+	selectEl.addEventListener('change', function() {
+  		let ind =  this.selectedIndex;
+  		//console.log(ind);
+  		for(let id of Object.getOwnPropertyNames(json))
+		{
+			let maker = json[id];			
+			if(maker.id == ind){
+				for(let key of maker.models){
+					//console.log(key.model_name);
+					let opt = document.createElement('option');
+					opt.appendChild(document.createTextNode(key.model_name));
+					opt.value = key;
+					selectElMod.appendChild(opt);
+				}
+			}
+		}
+
+	})
 	
 }
 else{
 	alert("Error HTTP!");
-}
+}	
